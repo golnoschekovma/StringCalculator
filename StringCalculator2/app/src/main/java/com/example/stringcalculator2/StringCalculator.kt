63d083b6bebcfe7046e8cc9dadae69af20ed6a0d
Mikhail Golnoshchekov <golnoschekovma@gmail.com>
2023-05-10 23:10:48 +0300

    Second implementation: item 2, 3, 4

--- StringCalculator2/app/src/main/java/com/example/stringcalculator2/StringCalculator.kt
+++ StringCalculator2/app/src/main/java/com/example/stringcalculator2/StringCalculator.kt
@@ -1,15 +1,22 @@
 package com.example.stringcalculator2
 
 class StringCalculator {
-    fun add(numbers: String): Int {
-        val delimiter = ","
+    fun add(_numbers: String): Int {
+        val delimiters = arrayOf(",", "\n").toMutableList()
+        var numbers = _numbers
         var sum = 0
-        return when {
-            numbers.isEmpty() -> 0
-            else -> {
-                numbers.split(delimiter).forEach { sum += it.toInt() }
-                sum
+
+        when {
+            numbers.isEmpty() -> return 0
+            numbers.length > 1 && numbers.substring(0..1) == "//" -> {
+                delimiters[0] = numbers[2].toString()
+                numbers = numbers.substringAfter("\n")
             }
         }
+
+        numbers.split(*delimiters.toTypedArray()).forEach {
+            sum += it.toInt()
+        }
+        return sum
     }
 }
--- StringCalculator2/app/src/test/java/com/example/stringcalculator2/StringCalculatorTest.kt
+++ StringCalculator2/app/src/test/java/com/example/stringcalculator2/StringCalculatorTest.kt
@@ -27,4 +27,28 @@ class StringCalculatorTest {
         val expected = 3
         assertEquals(expected, actual)
     }
+
+    @Test
+    fun unknownAmountOfIntegers_36() {
+        val stringCalculator = StringCalculator()
+        val actual = stringCalculator.add("1,2,3,4,5,6,7,8")
+        val expected = 36
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun linesBetweenNumbers_6() {
+        val stringCalculator = StringCalculator()
+        val actual = stringCalculator.add("1\n2,3")
+        val expected = 6
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun differentDelimiters_3() {
+        val stringCalculator = StringCalculator()
+        val actual = stringCalculator.add("//;\n1;2")
+        val expected = 3
+        assertEquals(expected, actual)
+    }
 }
