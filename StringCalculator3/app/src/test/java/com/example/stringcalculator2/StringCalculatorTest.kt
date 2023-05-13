70390fea6903b2311db1274cc6111d80245af336
Vasilii Maleev <keymusicman@gmail.com>
2023-05-13 19:31:42 +0200

    Add test for two numbers

--- StringCalculator3/app/src/test/java/com/example/stringcalculator2/StringCalculatorTest.kt
+++ StringCalculator3/app/src/test/java/com/example/stringcalculator2/StringCalculatorTest.kt
@@ -4,16 +4,26 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class StringCalculatorTest {
+    private val stringCalculator = StringCalculator()
+
     @Test
     fun `when "" should return 0`() {
-        val result = StringCalculator().add("")
+        val result = stringCalculator.add("")
 
         assertEquals(0, result)
     }
+
     @Test
     fun `when 1 should return 1`() {
-        val result = StringCalculator().add("1")
+        val result = stringCalculator.add("1")
 
         assertEquals(1, result)
+    }
+
+    @Test
+    fun `when 1,2 should return 3`() {
+        val result = stringCalculator.add("1,2")
+
+        assertEquals(3, result)
     }
 }
