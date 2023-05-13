b914619aabeaa84e192dc7dbe2120646308dd68a
Mikhail Golnoshchekov <golnoschekovma@gmail.com>
2023-05-13 20:37:51 +0300

    Test `when 1,2 should return 3` completed

--- StringCalculator3/app/src/main/java/com/example/stringcalculator2/StringCalculator.kt
+++ StringCalculator3/app/src/main/java/com/example/stringcalculator2/StringCalculator.kt
@@ -2,6 +2,12 @@ package com.example.stringcalculator2
 
 class StringCalculator {
     fun add(input: String): Int {
-         return if (input.isEmpty()) 0 else input.toInt()
+        return when {
+            input.isEmpty() -> 0
+            input.length == 1 -> input.toInt()
+            else -> {
+                input.split(",").sumOf { it.toInt() }
+            }
+        }
     }
 }
