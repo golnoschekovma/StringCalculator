ef6dfbff58f86936cf01110475e1250f20109499
Vasilij Maleev <keymusicman@gmail.com>
2023-05-13 19:09:19 +0200

    Add empty input test

--- StringCalculator3/app/src/test/java/com/example/stringcalculator2/StringCalculatorTest.kt
+++ StringCalculator3/app/src/test/java/com/example/stringcalculator2/StringCalculatorTest.kt
@@ -4,5 +4,11 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class StringCalculatorTest {
+    @Test
+    fun `when "" should return 0`() {
+        val result = StringCalculator().add("")
+
+        assertEquals(0, result)
+    }
 
 }
