725526f5bed93dfd7baa55e2ba28eef1818ac5d0
Mikhail Golnoshchekov <golnoschekovma@gmail.com>
2023-05-13 18:11:25 +0300

    Got rid of mutability

--- StringCalculator2/app/src/main/java/com/example/stringcalculator2/StringCalculator.kt
+++ StringCalculator2/app/src/main/java/com/example/stringcalculator2/StringCalculator.kt
@@ -1,20 +1,21 @@
 package com.example.stringcalculator2
 
 class StringCalculator {
-    fun add(_numbers: String): Int {
-        val delimiters = arrayOf(",", "\n").toMutableList()
-        var numbers = _numbers
+    fun add(input: String): Int {
+        val delimiters = arrayOf(",", "\n")
+        val numbers : String;
         var sum = 0
 
         when {
-            numbers.isEmpty() -> return 0
-            numbers.length > 1 && numbers.substring(0..1) == "//" -> {
-                delimiters[0] = numbers[2].toString()
-                numbers = numbers.substringAfter("\n")
+            input.isEmpty() -> return 0
+            input.length > 1 && input.substring(0..1) == "//" -> {
+                delimiters[0] = input[2].toString()
+                numbers = input.substringAfter("\n")
             }
+            else -> numbers = input
         }
 
-        numbers.split(*delimiters.toTypedArray()).forEach {
+        numbers.split(*delimiters).forEach {
             sum += it.toInt()
         }
         return sum
