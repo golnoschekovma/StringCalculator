2fc9888f6aaf675e5b636e78fda199c8e4d15d27
Vasilii Maleev <keymusicman@gmail.com>
2023-05-13 19:48:25 +0200

    Implement add for new line delimiter and refactor

--- StringCalculator3/app/src/main/java/com/example/stringcalculator2/StringCalculator.kt
+++ StringCalculator3/app/src/main/java/com/example/stringcalculator2/StringCalculator.kt
@@ -1,13 +1,5 @@
 package com.example.stringcalculator2
 
 class StringCalculator {
-    fun add(input: String): Int {
-        return when {
-            input.isEmpty() -> 0
-            input.length == 1 -> input.toInt()
-            else -> {
-                input.split(",").sumOf { it.toInt() }
-            }
-        }
-    }
+    fun add(input: String): Int = input.split(",", "\n").sumOf { it.toIntOrNull() ?: 0 }
 }
